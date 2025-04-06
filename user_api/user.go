255196ddcc3522d635672400632e9313bc015209
S7eFaNS <stefanikola298@gmail.com>
2025-04-07 01:48:41 +0200

    style: changed formatting

--- user_api/user.go
+++ user_api/user.go
@@ -9,7 +9,10 @@ import (
 
 func handleOK(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "200 Users! Current time is: %s", time.Now())
+
+	// function logged
 	log.Printf("Request received: Method: %s, Path: %s, Headers: %v\n", r.Method, r.URL.Path, r.Header)
+
 	w.WriteHeader(http.StatusOK)
 }
 
