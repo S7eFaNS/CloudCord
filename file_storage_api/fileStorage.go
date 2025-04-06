e5261182f7f439002f97ac6931d852eaa5433328
S7eFaNS <stefanikola298@gmail.com>
2025-04-07 01:52:40 +0200

    style: formatting again

--- file_storage_api/fileStorage.go
+++ file_storage_api/fileStorage.go
@@ -9,13 +9,18 @@ import (
 
 func handleOK(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "200 file storage! Current time is: %s", time.Now())
+
 	log.Printf("Request received: Method: %s, Path: %s, Headers: %v\n", r.Method, r.URL.Path, r.Header)
+
 	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	http.HandleFunc("/", handleOK) // 200 Ok
+
 	fmt.Println("Starting server on :8082...")
+
 	http.ListenAndServe(":8082", nil)
+
 	log.Fatal(http.ListenAndServe(":8082", nil))
 }
